353ad051fd7782ecf21f43a2368d0bdbf2a30bef
ashkan <ashkan.mokarian@gmail.com>
2023-07-11 15:29:25 +0200

    Format with black/Add poetry

---
+++ examples/test-model/README.md
@@ -0,0 +1,5 @@
+# Description
+
+This is a test model. Here all SIR states are available. S->I based on proximity.
+I->R after 50 model steps. Upon R they realease virions, but they don't do
+anything to the model.
--- infectio/visualization/matplot.py
+++ infectio/visualization/matplot.py
@@ -4,12 +4,7 @@ import matplotlib.pyplot as plt
 class Matplot:
     """Plots different visualizations using matplotlib.pyplot."""
 
-    def __init__(
-            self,
-            model,
-            State,
-            state_style_dict,
-            figsize=(14, 8)):
+    def __init__(self, model, State, state_style_dict, figsize=(14, 8)):
         self.fig = plt.figure(figsize=figsize)
         grid = plt.GridSpec(4, 7, wspace=0.01, hspace=0.01)
         self.ax_pos = self.fig.add_subplot(grid[:-1, :3])
@@ -17,8 +12,8 @@ class Matplot:
         self.ax_colorbar = self.fig.add_subplot(grid[:-1, -1], frameon=False)
         self.ax_lin = self.fig.add_subplot(grid[-1, :])
 
-        self.ax_pos.set_aspect('auto')
-        self.ax_dif.set_aspect('auto')
+        self.ax_pos.set_aspect("auto")
+        self.ax_dif.set_aspect("auto")
         self.colorbar = None
 
         self.model = model
@@ -26,7 +21,7 @@ class Matplot:
         self.state_style_dict = state_style_dict
         self.steps = []
         self.num_lists = {k: [] for k in self.State}
-    
+
     def update(self, step=None):
         # Initialize three lists for each cell state
         state_lists = {k: [] for k in self.State}
@@ -52,21 +47,25 @@ class Matplot:
             pos = [a.pos for a in state_lists[state]]
             if pos:
                 x, y = zip(*pos)
-                self.ax_pos.plot(x, y, linestyle='', **style)
+                self.ax_pos.plot(x, y, linestyle="", **style)
 
     def plot_lines(self, state_lists):
         """SIR number line plots"""
         self.ax_lin.cla()
         for k, v in self.num_lists.items():
             v.append(len(state_lists[k]))
-            self.ax_lin.plot(self.steps, v,
-                             '-' + self.state_style_dict[k]['color'],
-                             label=f'{k.value}: {v[-1]}')
-        self.ax_lin.legend(loc='center left')
-    
+            self.ax_lin.plot(
+                self.steps,
+                v,
+                "-" + self.state_style_dict[k]["color"],
+                label=f"{k.value}: {v[-1]}",
+            )
+        self.ax_lin.legend(loc="center left")
+
     def plot_particle(self):
-        self.ax_dif.cla() # This makes the program run much faster
+        self.ax_dif.cla()  # This makes the program run much faster
         diff_plot = self.ax_dif.imshow(
-            self.model.particle.u.T, vmin=0, vmax=1, origin='lower')
+            self.model.particle.u.T, vmin=0, vmax=1, origin="lower"
+        )
         if not self.colorbar:
             self.colorbar = self.fig.colorbar(diff_plot, ax=self.ax_colorbar)
