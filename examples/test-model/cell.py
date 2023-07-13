79b2ec2c5300e39c7787c351494448fcf9984a2c
ashkan <ashkan.mokarian@gmail.com>
2023-07-13 10:52:19 +0200

    Add all constant in test example to options file

--- examples/test-model/cell.py
+++ examples/test-model/cell.py
@@ -3,29 +3,26 @@ from enum import Enum
 
 import mesa
 
+import options as opt
+
 
 # Cell states
 class State(Enum):
-    S = 'Susceptible'
-    I = 'Infected'
-    R = 'Removed'
+    S = "Susceptible"
+    I = "Infected"
+    R = "Removed"
+
 
 def cell_speed(state: State):
-    return {
-        State.S: 1,
-        State.I: 5,
-        State.R: 0
-    }[state]
+    return {State.S: 1, State.I: 5, State.R: 0}[state]
 
-# Search radius for deciding cell-cell infection rate based on number of
-# infected cells in the radius
-CELL2CELL_INFECTION_RADIUS_SEARCH = 5
 
 # For now, let's choose a simple sigmoid function for deciding cell-cell infection chance
 def cell2cell_infection_chance(num_infected_neighbors):
     if num_infected_neighbors == 0:
         return 0.0
     return 1 / (1 + np.exp(-0.5 * (num_infected_neighbors - 5)))
+
 
 class Cell(mesa.Agent):
     """
@@ -38,20 +35,23 @@ class Cell(mesa.Agent):
 
         # Cell state
         self.state = State.S
+        # By having time_infected property for each cell, we don't need to have
+        # Multiple schedulers for each state. time_infected also becomes handy
+        # For other computations
         self.time_infected = None
-    
+
     @property
     def speed(self):
         return cell_speed(self.state)
 
     def move(self):
         """Random walk of cell based on speed."""
-        dx, dy = self.random.random()-0.5, self.random.random()-0.5
+        dx, dy = self.random.random() - 0.5, self.random.random() - 0.5
         velocity = np.array([dx, dy])
         velocity /= np.linalg.norm(velocity)
         new_pos = self.pos + velocity * self.speed
         self.model.space.move_agent(self, new_pos)
-    
+
     def infect_cell(self):
         assert self.state is State.S, "Only healthy cells can get infected."
         self.state = State.I
@@ -62,20 +62,26 @@ class Cell(mesa.Agent):
         if self.state is not State.S:
             return
         # Simple cell-cell infection only based on numbers
-        infected_neighbors = [c for c in self.model.space.get_neighbors(
-            self.pos, radius=CELL2CELL_INFECTION_RADIUS_SEARCH, include_center=False)
-            if c.time_infected]  # c.time_infected checks for both, if cell is
-                                 # infected, and also, if it wasn't decided to
-                                 # get infected in the current step
+        infected_neighbors = [
+            c
+            for c in self.model.space.get_neighbors(
+                self.pos,
+                radius=opt.CELL2CELL_INFECTION_RADIUS_SEARCH,
+                include_center=False,
+            )
+            if c.time_infected
+        ]  # c.time_infected checks for both, if cell is
+        # infected, and also, if it wasn't decided to
+        # get infected in the current step
         # TODO: what distribution to use and what parameters. For now, sigmoid
         # with linear exponentiation
         infection_prob = cell2cell_infection_chance(len(infected_neighbors))
         if infection_prob > self.random.random():
             self.infect_cell()
-    
+
     def add_virions_via_lysis(self):
         x, y = self.pos
-        self.model.particle.u[int(x), int(y)] += 20
+        self.model.particle.u[int(x), int(y)] += opt.PARTICLE_PRODUCTION_AT_LYSIS
 
     def kill_cell(self):
         assert self.state is State.I, "Only infected cells can die."
@@ -84,7 +90,7 @@ class Cell(mesa.Agent):
 
         # Cell lysis and changing model's virions at the position with a step function
         self.add_virions_via_lysis()
-    
+
     def decide_to_kill(self):
         if self.state is not State.I:
             return
@@ -98,7 +104,7 @@ class Cell(mesa.Agent):
         """
         self.decide_to_infect()
         self.decide_to_kill()
-    
+
     def advance(self) -> None:
         self.move()
         if self.state is State.I:
--- examples/test-model/options.py
+++ examples/test-model/options.py
@@ -1,1 +1,10 @@
 N_SIM_ITER = 500
+# Heat Diffusion Constants
+# all the coefficient used for homogenous diffusion in one.
+# \gamma = alpha * delta_t / delta_x ** 2 where alpha is the diffusion constant
+GAMMA = 0.2
+virions_diffusion_time_steps_per_each_model_step = 30
+# Search radius for deciding cell-cell infection rate based on number of
+# infected cells in the radius
+CELL2CELL_INFECTION_RADIUS_SEARCH = 5
+PARTICLE_PRODUCTION_AT_LYSIS = 10
