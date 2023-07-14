bf8283958e2340e9014815a821703a65e3d31d36
ashkan <ashkan.mokarian@gmail.com>
2023-07-14 10:40:40 +0200

    Add README notes and test example cleanup

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Infectio
 
 Agent-based modeling for Virological Plaque using Mesa library.
+
+# Personal notes
+
+* Can we avoid discretization of particle scalar fields and work completely with
+well defined function. For example solve the diffusion with it, and also for
+gradient computations be able to use [this python package: numdifftools](https://numdifftools.readthedocs.io/en/latest/index.html).
+
+* Assumption in code: x,y coords start from lower left. particle variable u also same, i.e. rows correspond with x and
+columns with y, first row is x=0 and first column is y=0. Therefore for ploting u, need to do
+plt.imshow(u.T, origin='lower')
+
+* Now, molecule released at int(x, y). Could also release with sub-pixel precision but not implemented.
--- examples/test-model/model.py
+++ examples/test-model/model.py
@@ -15,18 +15,10 @@ class TestModel(mesa.Model):
     def __init__(self, num_agents, width, height):
         super().__init__()
         self.num_agents = num_agents
-
-        # By having time_infected property for each cell, we don't need to have
-        # Multiple schedulers for each state. time_infected also becomes handy
-        # For other computations
-        # self.schedule = {state: mesa.time.SimultaneousActivation(self)
-        #                  for state in CellState}
         self.schedule = mesa.time.SimultaneousActivation(self)
-
         self.space = mesa.space.ContinuousSpace(
             x_max=width, y_max=height, torus=True
         )  # TODO: remove torus, also need to change cell.move()
-
         # Virions in space, used for molecular diffusion
         self.particle = Homogenous2dDiffusion(
             opt.GAMMA,
@@ -34,36 +26,23 @@ class TestModel(mesa.Model):
             height,
             opt.virions_diffusion_time_steps_per_each_model_step,
         )
-
+        # Initialize agents randomly
         for i in range(self.num_agents - 1):
             x = self.random.uniform(0, self.space.x_max)
             y = self.random.uniform(0, self.space.y_max)
             agent = Cell(i, self)
             self.schedule.add(agent)
             self.space.place_agent(agent, (x, y))
-
         # put an infected cell in the middle
         agent = Cell(i + 1, self)
         agent.infect_cell()
         self.space.place_agent(agent, (width / 2, height / 2))
         self.schedule.add(agent)
-
-        # example data collector
-        self.datacollector = mesa.DataCollector(
-            model_reporters={
-                State.I.value: lambda m: len(
-                    [c for c in m.schedule.agents if c.state is State.I]
-                )
-            }
-        )
-
         self.running = True
-        self.datacollector.collect(self)
 
     def step(self):
         """
         A model step. Used for collecting data and advancing the schedule
         """
-        self.datacollector.collect(self)
         self.particle.step()  # Virion diffusion step
         self.schedule.step()
