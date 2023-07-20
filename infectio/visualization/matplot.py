6fe5ba6343415c5fdd36f1bcf7bfc4dcb21c0ccb
ashkan <ashkan.mokarian@gmail.com>
2023-07-20 13:48:18 +0200

    Add radii linear plots

--- infectio/visualization/matplot.py
+++ infectio/visualization/matplot.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy.spatial import ConvexHull
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
@@ -5,13 +7,15 @@ import matplotlib.colors as mcolors
 class Matplot:
     """Plots different visualizations using matplotlib.pyplot."""
 
-    def __init__(self, model, State, state_style_dict, figsize=(14, 8)):
+    def __init__(self, model, State, state_style_dict, figsize=(14, 10)):
         self.fig = plt.figure(figsize=figsize)
-        grid = plt.GridSpec(4, 7, wspace=0.1, hspace=0.5)
-        self.ax_pos = self.fig.add_subplot(grid[:-1, :3])
-        self.ax_dif = self.fig.add_subplot(grid[:-1, 3:])
-        self.ax_colorbar = self.fig.add_subplot(grid[:-1, -1], frameon=False)
-        self.ax_lin = self.fig.add_subplot(grid[-1, :])
+        grid = plt.GridSpec(5, 7, wspace=0.1, hspace=0.5)
+        self.ax_pos = self.fig.add_subplot(grid[:3, :3])
+        self.ax_dif = self.fig.add_subplot(grid[:3, 3:])
+        self.ax_colorbar = self.fig.add_subplot(grid[:3, -1], frameon=False)
+        self.ax_lin_radius = self.fig.add_subplot(grid[3, :])
+        self.ax_lin_radius.set_xticks([])
+        self.ax_lin_count = self.fig.add_subplot(grid[4, :], sharex=self.ax_lin_radius)
 
         self.ax_pos.set_aspect("auto")
         self.ax_dif.set_aspect("auto")
@@ -31,6 +35,12 @@ class Matplot:
         self.steps = []
         self.num_lists = {k: [] for k in self.State}
 
+        # Using line plot to show radii mean, max, and min. To avoid keeping
+        # the data for previous steps, you need to use animations in pyplot
+        self.radii_mean = []
+        self.radii_max = []
+        self.radii_min = []
+
     def update(self, step=None):
         # Initialize three lists for each cell state
         state_lists = {k: [] for k in self.State}
@@ -39,7 +49,8 @@ class Matplot:
         if step is None:
             step = self.steps[-1] if self.steps else 0
         self.steps.append(step)
-        self.plot_lines(state_lists)
+        self.plot_lin_radius(state_lists[self.State.I])
+        self.plot_lin_count(state_lists)
         self.plot_particle()
 
         plt.draw()
@@ -54,18 +65,47 @@ class Matplot:
                 x, y = zip(*pos)
                 self.ax_pos.plot(x, y, linestyle="", **style)
 
-    def plot_lines(self, state_lists):
+    def plot_lin_radius(self, infected_cells):
+        """Radius line plots"""
+        self.ax_lin_radius.cla()
+        self.ax_lin_radius.set_xticks([])
+        if len(infected_cells) >= 3:
+            # Get list of infected cells on the boundary of the convex hull
+            points = [a.pos for a in infected_cells]
+            hull = ConvexHull(points)
+            outer_points = [points[i] for i in hull.vertices]
+            # Compute center of the convex hull
+            center = np.mean(outer_points, axis=0)
+            # Compute radii
+            aaa = outer_points - center
+            radii = np.linalg.norm(outer_points - center, axis=-1)
+
+        else:
+            # no convex hull for 1 or 2 infected cells
+            radii = [0]
+
+        self.radii_max.append(np.max(radii))
+        self.radii_mean.append(np.mean(radii))
+        self.radii_min.append(np.min(radii))
+
+        self.ax_lin_radius.plot(
+            self.steps,
+            np.array([self.radii_max, self.radii_mean, self.radii_min]).T,
+        )
+        self.ax_lin_radius.legend(["max", "mean", "min"], loc="center left")
+
+    def plot_lin_count(self, state_lists):
         """SIR number line plots"""
-        self.ax_lin.cla()
+        self.ax_lin_count.cla()
         for k, v in self.num_lists.items():
             v.append(len(state_lists[k]))
-            self.ax_lin.plot(
+            self.ax_lin_count.plot(
                 self.steps,
                 v,
                 "-" + self.state_style_dict[k]["color"],
                 label=f"{k.value}: {v[-1]}",
             )
-        self.ax_lin.legend(loc="center left")
+        self.ax_lin_count.legend(loc="center left")
 
     def plot_particle(self):
         self.ax_dif.cla()  # This makes the program run much faster
