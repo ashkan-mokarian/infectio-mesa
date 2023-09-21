af43a375740fc652c742f39da4d0fb8da74212c3
ashkan <ashkan.mokarian@gmail.com>
2023-09-21 11:06:55 +0200

    Realistic options.py. change ti 40000 diff steps.

--- examples/particle-gradient/options_debug.py
+++ examples/particle-gradient/options_debug.py
@@ -6,20 +6,20 @@ PARA_PRODUCE_T2 = 40
 GRADIENT_SPEED = 1
 RW_SPEED = 0.1
 GRADIENT_DIRECTION_NOISE_MAX_RADIAN = np.pi / 32
-
-# particle_diffusion_time_steps_per_each_model_step = 30
-particle_diffusion_time_steps_per_each_model_step = 40000
 # Heat Diffusion Constants
 # all the coefficient used for homogenous diffusion in one.
-# let alpha be 16.6E-7 cm2/s (reference: jamboard.page5)
 # \gamma = alpha * delta_t / delta_x ** 2 where alpha is the diffusion constant
-diffusion_delta_t = 10 * 60 / particle_diffusion_time_steps_per_each_model_step
-GAMMA = 16.6e-7 * diffusion_delta_t / (3.1746 * 1e-4) ** 2
+GAMMA = 0.2
+particle_diffusion_time_steps_per_each_model_step = 3
 # 48 hrs (duration of plaque experiments in dataset) * 6 (image sampling every
 # 10 minutes) ~= 300 images in total, almost half of the dataset duration which
 # where taken from 24h to 48h post infection.
-N_SIM_STEPS = 300
+N_SIM_STEPS = 30
 
 # Search radius for deciding cell-cell infection rate based on number of
 # infected cells in the radius
 CELL2CELL_INFECTION_RADIUS_SEARCH = 5
+
+# Saving params
+SAVE_ROOT = "./out/debug/"
+SAVE_NAME = None  # If None, will be date-time
