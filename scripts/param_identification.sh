054b0b25a6d0167e7e1ecc614f06a281f9337464
ashkan <ashkan.mokarian@gmail.com>
2023-11-27 14:52:28 +0100

    Add script for grid search paramter identification on cluster

--- scripts/param_identification.sh
+++ scripts/param_identification.sh
@@ -7,10 +7,7 @@ c2c_sigmoid_k=(1 2 3)
 c2c_sigmoid_x0=(2 3 4)
 c2c_radius_search=(4 8 15)
 
-# Specify the path to your second shell script
-second_script="second_script.sh"
-
-# Outer loop for randomwalk_speed
+# grid search for parameter identification
 for speed in "${randomwalk_speed[@]}"; do
     # Inner loop for gradient_speed
     for grad_speed in "${gradient_speed[@]}"; do
@@ -22,7 +19,7 @@ for speed in "${randomwalk_speed[@]}"; do
                 for radius_value in "${c2c_radius_search[@]}"; do
                     save_name="--save_name rw_speed=${speed}-grad_speed=${grad_speed}-c2c_k=${k_value}-c2c_x0=${x0_value}-c2c_radius=${radius_value}"
                     # Call the second script and pass all arguments
-                    sbatch ./scripts/cpuN1C2_param_identification.sbatch "--randomwalk_speed $speed" "--gradient_speed$grad_speed" "--c2c_sigmoid_k $k_value" "--c2c_sigmoid_x0 $x0_value" "--c2c_radius_search $radius_value"
+                    sbatch ./scripts/cpuN1C2_param_identification.sbatch $save_name "--randomwalk_speed" "$speed" "--gradient_speed" "$grad_speed" "--c2c_sigmoid_k $k_value" "--c2c_sigmoid_x0 $x0_value" "--c2c_radius_search $radius_value"
                 done
             done
         done
