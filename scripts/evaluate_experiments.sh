250001f79a654c3d05d0f9ac69b7a4b3df82a8fd
Ashkan Mokarian <ashkan.mokarian@gmail.com>
2023-12-12 13:48:16 +0100

    Dev evaluate (#3)
    * Add reference evaluation file
    
    * Add metric evaluation for experiments
    
    * Add bulk evaluation parallel coord plots

---
+++ scripts/evaluate_experiments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python ./infectio/evaluate.py \
+    --reference ./examples/vacv-epithelial/reference_metrics_for_M061_WR_handpicked.csv \
+    --root ./output/paridentify_0/ \
+    --tcol Infected_Count Mean_Radius \
+    --rmeancol inf-count-mean radius-mean \
+    --rstdcol inf-count-std radius-std \
+    --output ./output/paridentify_0/0_evaluation/bulk_evaluate.csv
