7c3e5ec4bfd3e8f91767ddd35ed53f0e41b4d016
ashkan <ashkan.mokarian@gmail.com>
2024-08-31 16:27:33 +0200

    Run multiple experiments in scripts

--- scripts/evaluate_experiments.sh
+++ scripts/evaluate_experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 python ./infectio/evaluate.py \
     --reference ./examples/vacv-epithelial/reference_metrics_for_M061_WR_handpicked.csv \
-    --root ./output/paridentify_0/ \
-    --tcol Infected_Count Mean_Radius \
-    --rmeancol inf-count-mean radius-mean(um) \
-    --rstdcol inf-count-std radius-std(um) \
-    --output ./output/paridentify_0/0_evaluation/bulk_evaluate.csv
+    --root ./output/CHANGE \
+    --tcol infected-count area\(um2\) radial-velocity\(um/min\) \
+    --rmeancol inf-count-mean area-mean\(um2\) radial-velocity-mean\(um/min\) \
+    --rstdcol inf-count-std area-std\(um2\) radial-velocity-std\(um/min\) \
+    --output ./output/CHANGE/0_evaluation/bulk_evaluate.csv
