b3e954a7a8c0ef1cd31a4b8f9fa2f8a353e5c126
ashkan <ashkan.mokarian@gmail.com>
2024-08-31 14:46:47 +0200

    add script to obtain top results from evaluations

--- scripts/sort_bulk_evaluate.py
+++ scripts/sort_bulk_evaluate.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import os
 
 # Replace 'your_file.csv' with the path to your CSV file
-file_path = "/Users/ashkanhzdr/workspace/infectio-mesa/output/dVGFdF11/1906/0_evaluation/bulk_evaluate.csv"
+root_path = "/Users/ashkanhzdr/workspace/infectio-mesa/output/dVGFdF11"
+file_path = os.path.join(root_path, "1907/0_evaluation/bulk_evaluate.csv")
+save_path = os.path.join(root_path, "1907/0_evaluation/top")
+os.makedirs(save_path, exist_ok=True)
 
 # Load the CSV file into a DataFrame
 df = pd.read_csv(file_path)
@@ -10,10 +14,15 @@ df = pd.read_csv(file_path)
 pd.set_option("display.max_colwidth", None)
 
 # Sort the DataFrame by the 'Infected_Count-dist' column in ascending order
-df_sorted = df.sort_values(by="Sum-dist")
+dist_cols = [col for col in df.columns if "-dist" in col]
+for col in dist_cols:
+    print(f"Sorting by {col}:")
+    df_sorted = df.sort_values(by=col)
+    print(df_sorted.head(5))
 
-# Get the top 5 rows with the smallest values in 'Infected_Count-dist'
-top_5_smallest = df_sorted.head(5)
-
-# Display the result
-print(top_5_smallest)
+    # copy the folders to a new location
+    tops = df_sorted.head(5)["target_folder"].tolist()
+    for top in tops:
+        src = os.path.join(root_path, top)
+        dst = os.path.join(save_path, top)
+        os.system(f"cp -r {src} {dst}")
